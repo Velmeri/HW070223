fe9ef372db197e5adbae305afa53b276aaf5fa75
Velmeri <danielmikhh2gmail.com>
2023-02-07 13:25:43 +0100

    Добавьте файлы проекта.

---
+++ HW070223/Store.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW070223
+{
+    internal class Store
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Store(string name, int year, string description, string phoneNumber, string email)
+        {
+            Name = name;
+            Year = year;
+            Description = description;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
---
+++ HW070223/Website.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW070223
+{
+    internal class Website
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Description { get; set; }
+        public string IP { get; set; }
+        public Website() { }
+        public Website(string name, string path, string description, string ip)
+        {
+            Name = name;
+            Path = path;
+            Description = description;
+            IP = ip;
+        }
+    }
+}
